645ad101bf4f1c4f9a01b29f9dfc23ab63d1310d
zach-rains <zrains723@gmail.com>
2025-01-11 15:28:14 -0800

    Delete Smoking+Gender.sql

--- Smoking+Gender.sql
+++
@@ -1,9 +0,0 @@
--- Question: Summarize heart disease counts by gender and smoking status.
-
-SELECT 
-    Gender,
-    Smoking,
-    COUNT(CASE WHEN Heart_Disease_Status = 'Yes' THEN 1 END) AS Heart_Disease_Count,
-    COUNT(*) AS Total_Patients
-FROM heart_disease_cleaned
-GROUP BY Gender, Smoking;
